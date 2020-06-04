910e1fa0eaea2b3d37a0ba5a7fe932979e1d5dd0
Sarah Wist <sarahwist107@gmail.com>
2020-06-03 20:58:21 -0700

    created boilerplate server.js and htmlRoutes.js code and began creating the 'Get', 'Post', and 'Delete' route code in apiRoutes.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
