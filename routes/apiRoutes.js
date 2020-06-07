5798bdaa5b9c226ff602341f8433e270eca67b9e
Sarah Wist <sarahwist107@gmail.com>
2020-06-06 17:19:19 -0700

    fixing routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,38 +11,40 @@ const { uuid } = require("uuidv4");
 // // ===============================================================================
 
 module.exports = function (app) {
-
   app.get("/api/notes", function (req, res) {
-    fs.readFile("db/db.json", (err, data) => {
+    fs.readFile("db/db.json", "utf8", (err, data) => {
       if (err) throw err;
-      console.log(data);
-      res.json(data);
+      res.json(JSON.parse(data));
     });
-});
+  });
 
-    app.post("/api/notes", function (req, res) {
-        fs.readFile("db/db.json",(err, data) => {
-            let input = req.body;
-            let jsonOutput = JSON.parse(data);
-            input.id = uuid();
-            jsonOutput.push(input);
-            fs.writeFile("db/db.json", JSON.stringify(jsonOutput, null, 2), err => {
-                if(err) throw err;
-                res.send(req.body);
-            });
+  app.post("/api/notes", function (req, res) {
+    fs.readFile("db/db.json", (err, data) => {
+      let input = req.body;
+      let jsonOutput = JSON.parse(data);
+      input.id = uuid();
+      jsonOutput.push(input);
+      fs.writeFile("db/db.json", JSON.stringify(jsonOutput, null, 2), (err) => {
+        if (err) throw err;
+        res.json(req.body);
       });
     });
+  });
 
-      app.delete("/api/notes", function (req, res) {
-        fs.readFile("db/db.json", (err, data) => { 
-            let input = JSON.parse(data);
-            let parsedOutput = input.filter(item => item.id != req.params.id);
-            fs.writeFile("db/db.json", JSON.stringify(parsedOutput, null, 2), err => {
-                if(err) throw err;
-                res.send(req.body)
-            })
-        });
-});
+  app.delete("/api/notes/:id", function (req, res) {
+    fs.readFile("db/db.json", (err, data) => {
+      let input = JSON.parse(data);
+      let parsedOutput = input.filter((item) => item.id != req.params.id);
+      fs.writeFile(
+        "db/db.json",
+        JSON.stringify(parsedOutput, null, 2),
+        (err) => {
+          if (err) throw err;
+          res.json(req.body);
+        }
+      );
+    });
+  });
 };
 
 //   } else {
