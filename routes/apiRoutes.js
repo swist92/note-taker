0db6b11b5659512b237c8099ae35cb4bfab38160
Sarah Wist <sarahwist107@gmail.com>
2020-06-08 00:01:49 -0700

    Updated README and removed unnecessary const from serves.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,3 @@
-// // ===============================================================================
 // // LOAD DATA
 // // We are linking our routes to a series of "data" sources.
 // // These data sources hold arrays of information on notes data.
@@ -8,7 +7,6 @@ const { uuid } = require("uuidv4");
 
 // // ===============================================================================
 // // ROUTING
-// // ===============================================================================
 
 module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
@@ -46,25 +44,3 @@ module.exports = function (app) {
     });
   });
 };
-
-//   } else {
-//     waitListData.push(req.body);
-//     res.json(false);
-//   }
-//   // API GET Requests
-//   // Below code handles when users "visit" a page.
-//   // In each of the below cases when a user visits a link
-//   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-//   // ---------------------------------------------------------------------------
-
-//   app.get("/api/tables", function(req, res) {
-//     res.json(tableData);
-//   });
-
-// API POST Requests
-// Below code handles when a user submits a form and thus submits data to the server.
-// In each of the below cases, when a user submits form data (a JSON object)
-// ...the JSON is pushed to the appropriate JavaScript array
-// (ex. User fills out a reservation request... this data is then sent to the server...
-// Then the server saves the data to the tableData array)
-// ---------------------------------------------------------------------------
--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
